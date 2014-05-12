8c10ea8b55e518ddef6ec116aa530e3033c24764
genjerator13 <root@genjerator.(none)>
2014-05-12 23:17:55 +0200

    current inventory

--- src/Numa/CIAdminBundle/Controller/ContainerAdminController.php
+++ src/Numa/CIAdminBundle/Controller/ContainerAdminController.php
@@ -7,10 +7,23 @@ use Sonata\AdminBundle\Controller\CRUDController as Controller;
 class ContainerAdminController extends Controller {
 
     public function listinAction() {
-        
-        return $this->listAction('aaa');
+        if (false === $this->admin->isGranted('LIST')) {
+            throw new AccessDeniedException();
+        }
+
+        $datagrid = $this->admin->getDatagrid();
+        $formView = $datagrid->getForm()->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($formView, $this->admin->getFilterTheme());
+        //\Doctrine\Common\Util\Debug::dump($datagrid->buildPager());
+        return $this->render($this->admin->getTemplate('list'), array(
+                    'action' => 'listin',
+                    'form' => $formView,
+                    'pagerurl' => 'listin',
+                    'datagrid' => $datagrid,
+                    'csrf_token' => $this->getCsrfToken('sonata.batch'),
+        ));
     }
-
-
 
 }
